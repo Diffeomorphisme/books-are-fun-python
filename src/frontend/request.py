a7840874c86aca2789b43a749df8c45d295c3672
Diffeomorphisme <nicosfaction@msn.com>
2025-02-26 10:00:50 +0100

    fix: adding error mgmt in frontend

--- src/frontend/request.py
+++ src/frontend/request.py
@@ -8,12 +8,16 @@ BACKEND_URL = "http://backend:3000/api/books"
 
 def get_all_books() -> pd.DataFrame | None:
     """Fetch the books from the backend"""
-    response = requests.get(BACKEND_URL)
-    if response.status_code != 200:
-        return
+    try:
+        response = requests.get(BACKEND_URL)
+        if response.status_code != 200:
+            return
 
-    results = response.json()
-    books = [
-        Book(**result) for result in results
-    ]
-    return pd.DataFrame([book.model_dump() for book in books])
+        results = response.json()
+        books = [
+            Book(**result) for result in results
+        ]
+        return pd.DataFrame([book.model_dump() for book in books])
+    except Exception as e:
+        print(f"Error fetching books: {e}")
+        return
